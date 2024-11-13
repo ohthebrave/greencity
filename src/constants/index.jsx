b3a3460e17f347b251bd38d33544115b9f21629e
George <georgejoj28@gmail.com>
2024-11-13 09:37:31 +0300

    remove testimonials

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -20,41 +20,41 @@ export const navItems = [
 
 export const testimonials = [
   {
-    user: "John Doe",
-    company: "Stellar Solutions",
+    user: "Enock Kamau",
+    company: "Alliance Survey Limited",
     image: user1,
     text: "The resources have been a tremendous help in making my lifestyle more eco-friendly. Plus, the campaigns are a fantastic way to get involved. Keep up the excellent work!",
   },
-  {
-    user: "Jane Smith",
-    company: "Blue Horizon Technologies",
-    image: user2,
-    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
-  },
-  {
-    user: "David Johnson",
-    company: "Quantum Innovations",
-    image: user3,
-    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
-  },
-  {
-    user: "Ronee Brown",
-    company: "Fusion Dynamics",
-    image: user4,
-    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
-  },
-  {
-    user: "Michael Wilson",
-    company: "Visionary Creations",
-    image: user5,
-    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
-  },
-  {
-    user: "Emily Davis",
-    company: "Synergy Systems",
-    image: user6,
-    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
-  },
+  // {
+  //   user: "Jane Smith",
+  //   company: "Blue Horizon Technologies",
+  //   image: user2,
+  //   text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+  // },
+  // {
+  //   user: "David Johnson",
+  //   company: "Quantum Innovations",
+  //   image: user3,
+  //   text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+  // },
+  // {
+  //   user: "Ronee Brown",
+  //   company: "Fusion Dynamics",
+  //   image: user4,
+  //   text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+  // },
+  // {
+  //   user: "Michael Wilson",
+  //   company: "Visionary Creations",
+  //   image: user5,
+  //   text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+  // },
+  // {
+  //   user: "Emily Davis",
+  //   company: "Synergy Systems",
+  //   image: user6,
+  //   text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+  // },
 ];
 
 export const features = [
